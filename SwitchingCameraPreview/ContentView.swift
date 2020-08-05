06a9a03e7e7e7878c49632171cc5dcdb8818af75
yogox <yogoyogox@gmail.com>
2020-08-05 22:04:07 +0900

    Initial Commit

---
+++ SwitchingCameraPreview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitchingCameraPreview
+//
+//  Created by yogox on 2020/08/05.
+//  Copyright © 2020 Yogox Galaxy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
